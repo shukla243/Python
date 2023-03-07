ae63b201b64f1140df91a28b86cdfff7def44fbd
Shukla G <Shukla@gmail.com>
2023-03-07 13:44:07 -0500

    Method overriding

---
+++ Overriding.py
@@ -0,0 +1,24 @@
+class A:
+
+    def fun1(self):
+        print('feature_1 of class A')
+
+    def fun2(self):
+        print('feature_2 of class A')
+
+
+class B(A):
+
+    def fun1(self):
+        print('Modified feature_1 of class A by class B')
+
+    def fun3(self):
+        print('feature_3 of class B')
+
+obj = B()
+
+obj.fun1()
+
+obj.fun2()
+
+obj.fun3()
