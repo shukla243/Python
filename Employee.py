50fc92a804fe28af6add86bdc1e2f8e699794ad4
Shukla G <Shukla@gmail.com>
2023-03-13 23:42:14 -0400

    Method overriding

---
+++ Employee.py
@@ -0,0 +1,26 @@
+class Employee():
+    count = 0
+    def __init__(self,*args,**kwargs):
+        self.name = args[0]
+        self.desig = args[1]
+        self.salary = kwargs['s']
+        Employee.count += 1
+
+    def Display(self):
+            print("There are %d employees" % Employee.count)
+    def Emp(self):
+            print("Name:",self.name,", Designation:",self.desig,", Salary:",self.salary)
+
+#e1 = ["Shukla","Manager",80000]
+#Emp(*e1)
+
+e1 = Employee('Shukla','Manager',s=80000)
+e1.Emp()
+
+e2 = Employee('Dakshayini','Team Lead',s=50000)
+e2.Emp()
+
+e2.Display()
+
+
+
